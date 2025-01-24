69b9e57508b109bfc59ee836f55a6a8707e7689c
Miriam Schulz <mirischulz@gmail.com>
2025-01-24 17:21:57 +0100

    add option to remove fast/invariant subjects; redistribute code from analysis to preprocessing

--- results/functions.R
+++ results/functions.R
@@ -3,13 +3,11 @@
 ### 22 January 2025
 
 
-### Libraries
+# Libraries
 library(tidyverse)
 library(Rmisc)
 library(ggplot2)
 
-
-### Functions for preprocessing
 
 # Read in PC Ibex data
 read.pcibex <- function(filepath, auto.colnames=TRUE, fun.col=function(col,cols){cols[cols==col]<-paste(col,"Ibex",sep=".");return(cols)}) {
@@ -54,9 +52,6 @@ convert_to_hours <- function(minutes) {
   return(formatted_time)
 }
 
-
-
-### Functions for analysis
 
 # Preprocess the data to the target regions etc.
 preprocess_data <- function(df) {
@@ -172,4 +167,79 @@ rt_hist <- function(dat, plot_subtitle) {
       plot.subtitle = element_text(size = 16, face = "italic", hjust = 0.5),
       strip.text = element_text(size = 18, face = "bold")
     ) 
+}
+
+# Remove subjects with too fast RTs in one task using the mean by-subject RT
+remove_fast_subjects <- function(df, threshold=200) {
+  nrow_orig <- nrow(df)
+  df_subj_mean <- df %>% 
+    filter(RT < 2500) %>%   # to remove very large outliers
+    summarySE(measurevar="RT",
+              groupvars=c("UniqueID", "Task"),
+              na.rm=T) %>% 
+    select(UniqueID, Task, RT, sd) %>% 
+    dplyr::rename(MeanRT = RT)
+  df_subj_fast <- df_subj_mean %>% 
+    filter(MeanRT < threshold)
+  print("Fast subjects to eliminate:")
+  (fast_subjects <- df_subj_fast$UniqueID)
+  df_subj_mean %>%
+    filter(UniqueID %in% fast_subjects) %>% 
+    print()
+  df <- df %>%
+    filter(!(UniqueID %in% fast_subjects))
+  nrow_new <- nrow(df)
+  nrow_diff <- nrow_orig-nrow_new
+  cat("Number of subjects eliminated:", length(fast_subjects), end="\n")
+  cat("Number of data points eliminated:", nrow_diff, "out of", nrow_orig)
+  cat("  (", round(nrow_diff / nrow_orig * 100, 2), "%)", sep="", end="\n")
+  return(df)
+}
+
+# Remove subjects with invariant RTs between production and comprehension
+remove_invavriant_subjects <- function(df, leniency = 10) {
+  nrow_orig <- nrow(df)
+  
+  # Get mean RT by Task and Subject
+  df_rt_by_task <- df %>%
+    dplyr::group_by(UniqueID, Task) %>%
+    dplyr::summarize(mean_RT = mean(RT, na.rm = TRUE), .groups = "drop")
+  
+  # Identify subjects whose RT in production > RT in comprehension
+  varying_subjects <- df_rt_by_task %>%
+    pivot_wider(names_from = Task, values_from = mean_RT) %>% # 
+    # dplyr::filter(Production > Comprehension + leniency |
+    #                 Comprehension > Production + leniency) %>%
+    dplyr::filter(Production > Comprehension + leniency) %>% 
+    pull(UniqueID)
+  
+  # Divide the data into participants with varying vs. invariant task RTs
+  
+  # a. varying
+  df_varying_subjects <- df %>%
+    filter(UniqueID %in% varying_subjects)
+  varying <- unique(df_varying_subjects$UniqueID)
+  print(length(varying))
+  
+  # b. invariant
+  df_invariant_subjects <- df %>% 
+    dplyr::filter(!(UniqueID %in% df_varying_subjects$UniqueID))
+  invariant <- unique(df_invariant_subjects$UniqueID)
+  print(length(invariant))
+  
+  nrow_new <- nrow(df_varying_subjects)
+  nrow_diff <- nrow_orig-nrow_new
+  
+  cat("Number of invariant subjects eliminated:", length(invariant), end="\n")
+  cat("Number of data points eliminated:", nrow_diff, "out of", nrow_orig)
+  cat("  (", round(nrow_diff / nrow_orig * 100, 2), "%)", sep="", end="\n\n")
+  
+  # Print mean RT by Task and Subject in the invariant group
+  cat("RTs of the invariant subjects:", end = "\n")
+  df_invariant_subjects %>%
+    dplyr::group_by(UniqueID, Task) %>%
+    dplyr::summarize(mean_RT = mean(RT, na.rm = TRUE), .groups = "drop") %>% 
+    print()
+  
+  return(df_varying_subjects)
 }
