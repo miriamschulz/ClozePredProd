86fc2ad951114b2c73a776a9c6ddf453f2c51394
Miriam Schulz <mirischulz@gmail.com>
2025-01-26 10:18:51 +0100

    add voice progress updates

--- results/whisper_wrapper.py
+++ results/whisper_wrapper.py
@@ -32,12 +32,12 @@ file_format = sys.argv[5]
 # Path to the folder
 
 # Get all folder names containing the keyword
-folders_with_keyword = [
+folders_with_keyword = sorted([
     folder_name
     for folder_name in os.listdir(path_in)
     if os.path.isdir(os.path.join(path_in, folder_name)) and
        keyword in folder_name
-]
+])
 
 # Define font colors to print to terminal
 green_font = "\033[1;32m"
@@ -54,22 +54,26 @@ else:
     sys.exit(1)
 
 # Add the path to the folders
-folders_with_keyword = sorted(folders_with_keyword)
-folderpaths = [path_in + f for f in folders_with_keyword]
+folderpaths = [path_in + folder_name for folder_name in folders_with_keyword]
 
-for f in folders_with_keyword:
-    print(f"├── {f}")
+for folder_name in folders_with_keyword:
+    print(f" ├── {folder_name}")
 
-print("\nSending folders to whisper for annotation...\n")
+print("\nSending folders to Whisper for annotation...\n")
 
-for p in folderpaths:
+for (i, folder_path) in enumerate(sorted(folderpaths)):
+
+    print(f"Starting annotations for subject {i+1} / {n_folders}.")
+    os.system(f"say 'Starting annotations for subject {i+1}.'")
 
     command = ["python", "whisper_transcriber.py",
-               model_size, p, path_out, file_format]
+               model_size, folder_path, path_out, file_format]
 
     # Call whisper trancription script
     try:
         subprocess.run(command, check=True)
-        print(f"\n{green_font}Wrapper script executed successfully.{reset_color}\n")
     except subprocess.CalledProcessError as e:
         print(f"{red_font}Error executing script: {e}{reset_color}")
+
+print(f"\n{green_font}Wrapper script executed successfully.{reset_color}\n")
+os.system("say 'Finished all annotations.'")
